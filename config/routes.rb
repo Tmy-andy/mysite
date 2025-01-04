a1c9b4a55fbbedd767cb6d2413dbe4c19a4b12d0
tmy300803 <tmy300803@gmail.com>
2025-01-04 13:40:06 +0700

    Cập nhật 0401250139

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # Đặt route gốc, trỏ về controller và action phù hợp
   root "products#index"
+  resources :products
 end
-
 
 
