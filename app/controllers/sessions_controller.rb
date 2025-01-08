a6481543a0c889d34338d843a4cf9041fa2a22b5
tmy300803 <tmy300803@gmail.com>
2025-01-08 11:38:49 +0700

    Update 0801251138

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,18 +26,4 @@ class SessionsController < ApplicationController
   def register_form
     @user = User.new
   end
-
-  # Xử lý đăng ký người dùng mới
-  def register_create
-    # Kiểm tra thông tin đầu vào và tạo người dùng
-    if params[:email_address].present? && params[:password].present?
-      # Tạo tài khoản người dùng (hoặc dùng session nếu không có model User)
-      session[:user] = { email: params[:email_address], password: params[:password] }
-      
-      # Sau khi đăng ký thành công, chuyển hướng đến trang đăng nhập
-      redirect_to new_session_path, notice: "Account created successfully. Please log in."
-    else
-      redirect_to signup_path, alert: "Please provide an email and password."
-    end
-  end
 end
