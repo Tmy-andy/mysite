0336bb136587550f3b2428761711054aa445980c
tmy300803 <tmy300803@gmail.com>
2025-01-09 23:53:53 +0700

    Update09012353

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "trix"
-import "@rails/actiontext"
+import "trix";
+import "@rails/actiontext";
 import Rails from "@rails/ujs";
-Rails.start();
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-  
-  
+Rails.start();
